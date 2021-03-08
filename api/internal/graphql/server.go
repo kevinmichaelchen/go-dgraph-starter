c78d44ba8dacfcca11c585e5f1d2000570c49805
Kevin Chen <kevinmichaelchen@gmail.com>
2021-03-07 19:20:11 -0500

    Fix CORS issue

--- api/internal/graphql/server.go
+++ api/internal/graphql/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 
 	"github.com/MyOrg/go-dgraph-starter/internal/configuration"
@@ -28,19 +30,49 @@ func NewServer(config configuration.Config, service service.Service) Server {
 }
 
 func (s Server) Run() {
-	h := handler.New(&handler.Config{
+	r := mux.NewRouter()
+
+	graphqlHandler := handler.New(&handler.Config{
 		Schema:   s.buildSchema(),
 		Pretty:   true,
 		GraphiQL: true,
 	})
 
-	http.Handle("/graphql", h)
+	wrapWithCors := handlers.CORS(
+		handlers.AllowedOrigins([]string{
+			"http://localhost:3000",
+		}),
+		handlers.AllowedMethods([]string{
+			http.MethodGet,
+			http.MethodHead,
+			http.MethodPost,
+			// http.MethodOptions,
+		}),
+		handlers.AllowedHeaders([]string{
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Language",
+			"Access-Control-Request-Headers",
+			"Access-Control-Request-Method",
+			"Authorization",
+			"Connection",
+			"Content-Language",
+			"Content-Type",
+			"Host",
+			"Origin",
+			"Referer",
+			"User-Agent",
+		}),
+		handlers.AllowCredentials(),
+	)
+
+	r.Handle("/graphql", graphqlHandler)
 
 	address := fmt.Sprintf(":%d", s.config.HTTPPort)
 
 	log.Info().Msgf("Starting %s on %s...", s, address)
 
-	if err := http.ListenAndServe(address, nil); err != nil {
+	if err := http.ListenAndServe(address, wrapWithCors(r)); err != nil {
 		log.Fatal().Err(err).Msgf("Failed to serve %s on address: %s", s, address)
 	}
 }
