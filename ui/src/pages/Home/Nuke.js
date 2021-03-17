f75f07aec96e183564c818c8738bea895afeedc6
Kevin Chen <kevinmichaelchen@gmail.com>
2021-03-17 12:49:07 -0400

    refetchQueries

--- ui/src/pages/Home/Nuke.js
+++ ui/src/pages/Home/Nuke.js
@@ -1,4 +1,4 @@
-import { Button, IconButton, Box } from "@chakra-ui/react";
+import { Button, Box, useToast } from "@chakra-ui/react";
 import {
   Modal,
   ModalOverlay,
@@ -10,18 +10,65 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import { useMutation } from "@apollo/client";
-import { Mutations } from "../../graphql";
+import { Mutations, Queries } from "../../graphql";
 
 export const Nuke = () => {
   const [nukeData, { loading, error }] = useMutation(Mutations.NUKE);
+  const toast = useToast();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const handleNukeClick = () => {
-    nukeData();
-    onClose();
+  if (error) {
+    toast({
+      title: "Oops.",
+      position: "top-right",
+      description: JSON.stringify(error),
+      status: "error",
+      duration: 4000,
+      isClosable: true,
+    });
+  }
 
-    // TODO Todo List is not currently re-rendering. You'd expect it to re-render with zero items.
+  const handleNukeClick = () => {
+    // Perform the GraphQL mutation to drop data.
+    // The update field should instruct Apollo to modify its cache,
+    // overwriting its internal data with an empty object.
+    nukeData({
+      // An array or function that allows you to specify which queries
+      // you want to refetch after a mutation has occurred.
+      // Array values can either be queries (with optional variables)
+      // or just the string names of queries to be refeteched.
+      refetchQueries: [
+        {
+          query: Queries.GET_TODOS_QUERY,
+          variables: {
+            first: 10,
+            after: "",
+          },
+        },
+      ],
+      // A function used to update the cache after a mutation occurs
+      update: (cache) => {
+        cache.modify({
+          fields: {
+            allTodos() {
+              return {
+                edges: [],
+                pageInfo: {
+                  hasPreviousPage: false,
+                  hasNextPage: true,
+                  startCursor: "",
+                  endCursor: "",
+                },
+              };
+            },
+          },
+        });
+      },
+    });
+
+    // Close the modal
+    onClose();
   };
 
   return (
