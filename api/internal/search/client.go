16d9c8bdf326ad67aa42caabf163e7b8054a5b87
Kevin Chen <kevinmichaelchen@gmail.com>
2021-03-18 23:31:48 -0400

    Add function to query search index

--- api/internal/search/client.go
+++ api/internal/search/client.go
@@ -2,6 +2,7 @@ package search
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/MyOrg/go-dgraph-starter/internal/obs"
@@ -13,8 +14,11 @@ const (
 	indexForTodos = "todos"
 )
 
+type TodoID string
+
 type Client interface {
 	AddOrUpdate(ctx context.Context, todo *todoV1.Todo) error
+	Query(ctx context.Context, query string) ([]TodoID, error)
 }
 
 type impl struct {
@@ -41,8 +45,71 @@ func (i *impl) AddOrUpdate(ctx context.Context, todo *todoV1.Todo) error {
 	return nil
 }
 
+func (i *impl) Query(ctx context.Context, query string) ([]TodoID, error) {
+	// Search the index
+	res, err := i.client.Search(indexForTodos).Search(meilisearch.SearchRequest{
+		Query: query,
+		Limit: 10,
+	})
+
+	// Handle error
+	if err != nil {
+		return nil, fmt.Errorf("failed to search index: %w", err)
+	}
+
+	todos, err := hitsToProtobufs(ctx, res.Hits)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert search results to protobufs: %w", err)
+	}
+
+	var ids []TodoID
+	for _, todo := range todos {
+		ids = append(ids, TodoID(todo.Id))
+	}
+
+	return ids, nil
+}
+
 func todoToDocument(ctx context.Context, todo *todoV1.Todo) []map[string]interface{} {
 	return []map[string]interface{}{
 		{"todo_id": todo.Id, "title": todo.Title},
 	}
+}
+
+func hitsToProtobufs(ctx context.Context, hits []interface{}) ([]*todoV1.Todo, error) {
+	var out []*todoV1.Todo
+
+	for _, hit := range hits {
+		pb := &todoV1.Todo{}
+		if hitMap, ok := hit.(map[string]interface{}); !ok {
+			return nil, errors.New("could not convert hit to map")
+		} else {
+
+			// Get the ID
+			if id, ok := hitMap["todo_id"]; !ok {
+				return nil, errors.New("todo_id not present in search hit")
+			} else {
+				if idString, ok := id.(string); !ok {
+					return nil, errors.New("todo_id not in search hit is not a string")
+				} else {
+					pb.Id = idString
+				}
+			}
+
+			// Get the title
+			if title, ok := hitMap["title"]; !ok {
+				return nil, errors.New("title not present in search hit")
+			} else {
+				if titleString, ok := title.(string); !ok {
+					return nil, errors.New("title not in search hit is not a string")
+				} else {
+					pb.Title = titleString
+				}
+			}
+
+		}
+
+		out = append(out, pb)
+	}
+	return out, nil
 }
